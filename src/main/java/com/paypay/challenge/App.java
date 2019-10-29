16fa529a29736b50ef002addb8197152532c936b
hoang-tranxuan <hoangtx.social@gmail.com>
2019-10-29 11:18:16 +0900

    Initialize project

---
+++ src/main/java/com/paypay/challenge/App.java
@@ -0,0 +1,11 @@
+package com.paypay.challenge;
+
+/**
+ * Defines entry method of the application. This application is to demonstrate
+ * immutable queue implementation.
+ */
+public class App {
+    public static void main(String[] args) {
+	System.out.println("Immutable Queue Application");
+    }
+}
